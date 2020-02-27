291de1ff1f15d672ec75a813240f1b466b85c326
sanju s nair <sanju.nair4@cognizant.com>
2020-02-28 01:01:42 +0530

    db change

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,6 +1,7 @@
 package io.pivotal.pal.tracker;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +13,15 @@ import javax.sql.DataSource;
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Autowired
+    DataSource dataSource;
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
     public TimeEntryRepository create() {
-        return new JdbcTimeEntryRepository(createDataSource());
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     DataSource createDataSource(){
